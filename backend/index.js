4dfdc2a2423f0696d3dcd2fe44070dd276f51944
Sanika Walunj <sanikawalunj@gmail.com>
2026-05-01 04:18:32 +0530

    message

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,23 @@
+// const express = require("express");
+// const cors = require("cors");
+
+// const leadRoutes = require("./routes/leadRoutes");
+
+// const app = express();
+
+// app.use(cors());
+// app.use(express.json());
+
+
+// app.use("/leads", leadRoutes);
+
+// app.listen(5000, () => {
+//   console.log("Server running on port 5000");
+// });
+
 const express = require("express");
 const cors = require("cors");
+const { Pool } = require("pg");
 
 const leadRoutes = require("./routes/leadRoutes");
 
@@ -8,9 +26,22 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// use routes
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+
+app.set("db", pool);
+
+
 app.use("/leads", leadRoutes);
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
--- backend/routes/leadRoutes.js
+++ backend/routes/leadRoutes.js
@@ -1,16 +1,63 @@
+// const express = require("express");
+// const router = express.Router();
+
+// const {
+//   getLeads,
+//   createLead,
+//   updateLead,
+//   deleteLead,
+// } = require("../controllers/leadController");
+
+// router.get("/", getLeads);
+// router.post("/", createLead);
+// router.put("/:id", updateLead);
+// router.delete("/:id", deleteLead);
+
+// module.exports = router;
+
 const express = require("express");
 const router = express.Router();
 
-const {
-  getLeads,
-  createLead,
-  updateLead,
-  deleteLead,
-} = require("../controllers/leadController");
+// GET all leads
+router.get("/", async (req, res) => {
+  const db = req.app.get("db");
+  const result = await db.query("SELECT * FROM leads ORDER BY id ASC");
+  res.json(result.rows);
+});
 
-router.get("/", getLeads);
-router.post("/", createLead);
-router.put("/:id", updateLead);
-router.delete("/:id", deleteLead);
+// ADD lead
+router.post("/", async (req, res) => {
+  const db = req.app.get("db");
+  const { name, phone, source } = req.body;
+
+  await db.query(
+    "INSERT INTO leads (name, phone, source) VALUES ($1, $2, $3)",
+    [name, phone, source]
+  );
+
+  res.send("Lead added");
+});
+
+// UPDATE
+router.put("/:id", async (req, res) => {
+  const db = req.app.get("db");
+  const { status } = req.body;
+
+  await db.query(
+    "UPDATE leads SET status=$1 WHERE id=$2",
+    [status, req.params.id]
+  );
+
+  res.send("Updated");
+});
+
+// DELETE
+router.delete("/:id", async (req, res) => {
+  const db = req.app.get("db");
+
+  await db.query("DELETE FROM leads WHERE id=$1", [req.params.id]);
+
+  res.send("Deleted");
+});
 
 module.exports = router;
